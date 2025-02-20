520099e0c3f1dc0e74162dc370081ab05e1707e4
William Ravel <wravel@gmail.com>
2025-02-20 18:12:31 +0100

    Refactor content structure, enhance styles, and update Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,njk,css}", "./src/_includes/*.{html,njk,css}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        "fira-code": ['"Fira Code"', "monospace"],
-      },
-    },
-  },
+  content: ["./src/**/*.{html,njk,css}", "./src/_includes/**/*.{html,njk,css}"],
+  // theme: {
+  //   extend: {
+  //     fontFamily: {
+  //       "fira-code": ['"Fira Code"', "monospace"],
+  //     },
+  //   },
+  // },
   plugins: [],
 };
