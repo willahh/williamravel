6a0a5957682803fd898f935dd00191949ce9265a
William Ravel <wravel@gmail.com>
2025-02-20 11:11:21 +0100

    Add new versioned templates and update Eleventy configuration

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/images'); // Copie tout le dossier images
   return {
     dir: {
-      input: 'src',
+      input: 'src/v1',
       includes: '_includes', // Dossier des partials
       output: '_site',
     },
